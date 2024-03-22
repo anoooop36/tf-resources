18d64d449362138a78d63d350b50f23ab4150805
Anoop Chaudhary <anoopchaudhary36@gmail.com>
2024-03-22 16:03:53 +0530

    Update main.tf

--- gcp/vm/main.tf
+++ gcp/vm/main.tf
@@ -14,45 +14,77 @@ provider "google" {
   zone	      = var.zone
 }
 
-resource "google_sql_database_instance" "main" {
-  name             = var.name
-  database_version = var.database_version
-  region           = var.region
-  deletion_protection = var.deletion_protection
+resource "google_compute_instance" "vm_instance" {
+  name         = var.vm_name
+  machine_type = "e2-micro"
 
-  settings {
-    # Second-generation instance tiers are based on the machine
-    # type. See argument reference below.
-    tier = var.tier
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network = google_compute_network.vpc_network.self_link
+    access_config {
+    }
   }
 }
 
-variable "deletion_protection" {
-  default = false
+resource "google_compute_network" "vpc_network" {
+  name                    = var.net_name
+  auto_create_subnetworks = "true"
+}
+
+resource "google_compute_disk" "default" {
+  name  = var.name
+  type  = var.type
+  zone  = var.zone
+  image = var.image
+  labels = {
+    environment = "dev"
+  }
+  size = var.size
+  physical_block_size_bytes = var.block_size
+}
+
+variable "vm_name" {
+  default = "tf-instance-removeit"
+}
+
+variable "net_name" {
+  default = "tf-network-removeit"
+}
+
+variable "type" {
+  default = "pd-standard"
 }
 
 variable "name" {
-  default = "main-instance-removeit"
+  default = "test-disk-removeit"
 }
 
-
-variable "database_version" {
-  default = "POSTGRES_15"
+variable "zone" {
+  default = "us-central1-a"
 }
 
-variable "tier" {
-  default = "db-f1-micro"
+variable "image" {
+  default = "debian-11-bullseye-v20220719"
+}
+
+variable "size" {
+  default = 20
+}
+
+variable "block_size" {
+  default = 4096
 }
 
 variable "project" {}
 
 variable "region" {
 	default = "us-central1"
-}
-
-
-variable "zone" {
-  default = "us-central1-a"
 }
 
 variable "credentials" {}
